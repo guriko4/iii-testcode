e2cd8f7bdcece1fb02720109a4bc1857e82b74d7
Iikai <iikai.noriko@gmail.com>
2025-07-01 18:33:01 +0900

    7/1業務完了分

--- gsap-test/src/main.js
+++ gsap-test/src/main.js
@@ -1,9 +1,28 @@
 import './style.scss';
 import { gsap } from 'gsap';
 
-gsap.to('.box', {
-  x: 200,
-  rotation: 360,
-  duration: 2,
-  ease: 'power2.inOut',
+document.querySelector('.stage').addEventListener('click', () => {
+  const tl = gsap.timeline();
+  tl.to('#woman-arm', {
+    rotation: 24,
+    transformOrigin: '31% 78%',
+    duration: 0.5
+  })
+    .to('#left-comment', {
+      scale: 1,
+      opacity: 1,
+      duration: .4,
+      ease: "back.out(2.5)"
+    })
+    .to('#arrow', {
+      opacity: 1,
+      x: -20,
+      duration: 1,
+      ease: "back.out(1.7)"
+    })
+    .to('#img-icon', {
+      scale: 1,
+      x: 0,
+      duration: .6,
+    })
 });
